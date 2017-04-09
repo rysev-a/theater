e6744a128136fe791b773d455d68665416106ec6
alex <rysev-a@yandex.ru>
2017-04-09 12:59:13 +0300

    add wizard, wizard.profile module

---
+++ client/app/wizard/profile/profile.component.js
@@ -0,0 +1,8 @@
+angular.
+  module('wizard.profile').
+  component('profile', {
+    templateUrl: 'wizard/profile/profile.template.html',
+    controller: [
+      function ProfileController() {}
+    ]
+  });
